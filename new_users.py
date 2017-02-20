804e044bbd30a61dbbb689553fd72d815b7a57a0
João Eduardo Montandon <edu.montandon@gmail.com>
2017-02-20 11:15:37 -0300

    Added students directory creation.

---
+++ new_users.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import sys
+import os
+import dropbox
+
+def main(argv):
+
+    if len(argv) < 3:
+        print "Error: arguments not provided"
+        exit(1)
+
+    fileName = argv[1]
+    path = argv[2]
+
+    with open(fileName) as file: 
+        for line in file:
+            username = line.split("\t")[0]
+            email = line.split("\t")[1]
+
+            print "Processing student: " + username
+            createFolder(path, username)
+
+def createFolder(path, username):
+    """Create a file
+
+    Create a file using base path and username.
+    Returns the folder's name if operation was successful
+    """
+    username = username.decode("utf-8").upper()
+
+    filePath = path + "/" + username
+    if not os.path.exists(filePath):
+        os.makedirs(filePath)
+        return filePath
+    return None
+
+if __name__ == "__main__":
+    main(sys.argv)
